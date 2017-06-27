6fa7c9895b92439e4312e533cb3b19efe52b327f
Max Irvine <mirvine46@gmail.com>
2017-06-27 14:29:25 -0400

    Adds updated style

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
 # points awarded based upon time
 import pygame, sys, time
+import pygame.gfxdraw
 from pygame.locals import *
-
-def keyboard_input(self):
-    key = pygame.key.get_pressed()
-    dist = 1
-    if key[pygame.K_LEFT]:
-        self.rect.move(-1, 0)
-    if key[pygame.K_RIGHT]:
-        self.rect.move(1, 0)
-    if key[pygame.K_UP]:
-        self.rect.move(0, -1)
-    if key[pygame.K_DOWN]:
-        self.rect.move(0, 1)
 
 # main loop of game
 def play(window_size, surface, black, blue, red):
@@ -23,8 +12,8 @@ def play(window_size, surface, black, blue, red):
 
     print ball_height
     pygame.draw.line(surface, black, (middle, 0), (middle, window_size), 5)
-    pygame.draw.circle(surface, blue, (358, ball_height), radius, 0)
-    pygame.draw.circle(surface, red, (443, ball_height), radius, 0)
+    pygame.gfxdraw.filled_circle(surface, 357, ball_height, radius, blue)
+    pygame.gfxdraw.filled_circle(surface, 443, ball_height, radius, red)
 
     while True:
         for event in pygame.event.get():
@@ -44,13 +33,13 @@ def main():
 
     # set up the colors
     # balls red and blue
-    # platform gray
+    # platform light_blue
     # deadly triangles black
-    black = (0, 0, 0)
-    gray  = (77, 77, 77)
-    white = (255, 255, 255)
-    red = (255, 0, 0)
-    blue = (0, 0, 255)
+    black = (12, 18, 12)
+    light_blue  = (168, 218, 220)
+    white = (241, 250, 238)
+    red = (230, 57, 70)
+    blue = (35, 87, 137)
     surface.fill(white)
 
     play(window_size, surface, black, blue, red)
