dd766521e07d9a8007bf79ce58f1a1ac31d34b9f
Max Irvine <mirvine46@gmail.com>
2017-06-27 15:48:07 -0400

    End of day 06/27/2017

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 # points awarded based upon time
-import pygame, sys, time
+import pygame, sys
 import pygame.gfxdraw
 from pygame.locals import *
 
-# main loop of game
-def play(window_size, surface, black, blue, red):
-    #contains main loop
+def moveBlue(pos):
+    count = 0
+    clock = pygame.time.Clock()
+    for x in range (0, 100):
+        clock.tick(360)
+        count += 1
+        pos -= count
+    return pos
+
+
+def play(window_size, surface, black, blue, red, white):
     radius = 40
     ball_height = window_size - (radius + 1) 
     middle = window_size / 2
+    blue_x = 357
+    red_x = 443
 
-    print ball_height
-    pygame.draw.line(surface, black, (middle, 0), (middle, window_size), 5)
-    pygame.gfxdraw.filled_circle(surface, 357, ball_height, radius, blue)
-    pygame.gfxdraw.filled_circle(surface, 443, ball_height, radius, red)
-
+    # main loop of game
     while True:
+        surface.fill(white)
+        pygame.draw.line(surface, black, (middle, 0), (middle, window_size), 5)
+        pygame.gfxdraw.filled_circle(surface, blue_x, ball_height, radius, blue)
+        pygame.gfxdraw.filled_circle(surface, red_x, ball_height, radius, red)
         for event in pygame.event.get():
             if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
-        pygame.display.update()
+            if event.type == KEYDOWN:
+                if (event.key == K_LEFT):
+                    moveBlue(blue_x)
+                elif (event.key == K_RIGHT):
+                    red_x += 10
+            pygame.display.update()
     
 def main():
     # set up pygame
@@ -40,9 +55,9 @@ def main():
     white = (241, 250, 238)
     red = (230, 57, 70)
     blue = (35, 87, 137)
-    surface.fill(white)
+    
 
-    play(window_size, surface, black, blue, red)
+    play(window_size, surface, black, blue, red, white)
 
 
 main()
